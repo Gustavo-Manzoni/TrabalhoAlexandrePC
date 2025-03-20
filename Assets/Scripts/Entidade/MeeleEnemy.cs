a876a8596c80fc8c1ae3b1d4f5de179a89dab8db
Gustavo-Manzoni <gustavomanzoni2008@gmail.com>
2025-03-20 08:48:36 -0300

    Finished meele enemy animations

--- Assets/Scripts/Entidade/MeeleEnemy.cs
+++ Assets/Scripts/Entidade/MeeleEnemy.cs
@@ -8,11 +8,19 @@ public class MeeleEnemy : MonoBehaviour, IMovable
     [SerializeField] BaseEnemy baseEnemy;
     Transform target;
     NavMeshAgent agent;
+    Animator anim;
+    bool lookingRight;
+    float life;
     void Start()
     {
+        anim = GetComponent<Animator>();
         agent = GetComponent<NavMeshAgent>();
+        agent.speed = baseEnemy.speed;
         agent.updateRotation = false;
         agent.updateUpAxis = false;
+        life = baseEnemy.maxLife;
+
+        lookingRight = true;
     }
 
     
@@ -20,11 +28,30 @@ public class MeeleEnemy : MonoBehaviour, IMovable
     { 
         if(target == null) return;  
         agent.SetDestination(target.position);
+        UpdateAnimator();
 
-
+    }
+    void UpdateAnimator() 
+    {
+        Vector2 direction = -(transform.position - target.position).normalized;
+        float horizontal = Mathf.Round(direction.x);
+        float vertical = Mathf.Round(direction.y);
+        anim.SetFloat("Horizontal", horizontal);
+        anim.SetFloat("Vertical", vertical);
+        if(lookingRight && horizontal < 0 || !lookingRight && horizontal > 0) 
+        {
+            lookingRight = !lookingRight;
+            Vector2 newScale = (Vector2)transform.localScale;
+            newScale.x *= -1;
+            transform.localScale = newScale;
+        
+        
+        
+        }
     }
     public void Move()
     {
         target = GameObject.FindGameObjectWithTag("Player").transform;
+        anim.SetBool("Running", true);
     }
 }
