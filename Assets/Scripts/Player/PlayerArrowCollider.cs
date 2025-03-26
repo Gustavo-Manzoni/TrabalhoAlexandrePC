2700f061b2076da4378b84de949f053fe97fa776
Gustavo-Manzoni <gustavomanzoni2008@gmail.com>
2025-03-26 11:39:16 -0300

    PlayerArrow collider and damage

---
+++ Assets/Scripts/Player/PlayerArrowCollider.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerArrowCollider : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+
+
+        if (collision.gameObject.TryGetComponent(out IDamageable target) && collision.gameObject.tag != "Player")
+        {
+            target.TakeDamage(4);
+            Destroy(gameObject);
+
+        }
+        if (collision.gameObject.TryGetComponent(out IKnockbackable targetToKnockback) && collision.gameObject.tag != "Player")
+        {
+            Vector2 direction = (Vector2)transform.position - (Vector2)collision.gameObject.transform.position;
+            InventoryManager.instance.StartCoroutine(targetToKnockback.TakeKnockback(0.25f, 19, -direction.normalized));
+            Destroy(gameObject);
+            //Professor você pode julgar meus metodos mas não meus resultados kkkkkk
+            //eu so passei a responsabilidade de chamar a
+            //corroutina para uma instancia que eu ja criei para não precisar
+            //criar uma nova e considerando que o invcentario sempre vai existir
+
+
+        }
+    
+
+    }
+}
